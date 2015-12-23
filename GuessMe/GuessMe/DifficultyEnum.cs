f7d9a62bd58ebec98e2fbb472ff76c4db9588902
Josipaaa <josipa.popovic@fer.hr>
2015-12-23 15:04:21 +0100

    Nedovrsena verzija GUIja za aplikaciju GuessMe.

---
+++ GuessMe/GuessMe/DifficultyEnum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMe {
+    public sealed class DifficultyEnum {
+
+        private readonly string difficulty;
+        private readonly int difficultyValue;
+
+        public static readonly DifficultyEnum EASY = new DifficultyEnum(0, "EASY");
+        public static readonly DifficultyEnum MEDIUM = new DifficultyEnum(1, "MEDIUM");
+        public static readonly DifficultyEnum HARD = new DifficultyEnum(2, "HARD");
+
+        private DifficultyEnum(int difficultyValue, string difficulty) {
+
+            this.difficulty = difficulty;
+            this.difficultyValue = difficultyValue;
+
+        }
+
+        public override string ToString() {
+
+            return difficulty;
+        }
+    }
+}
---
+++ GuessMe/GuessMe/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMe {
+    public class Team {
+
+        private string _teamName;
+        private float _points = 0.0f;
+        private int _guessed = 0;
+        private int _notGuessed = 0;
+
+        public string TeamName { get; set; }
+        public float Points { get; set; }
+        public int Guessed { get; set; }
+        public int NotGuessed { get; set;}
+
+        public Team(string TeamName) {
+            _teamName = TeamName;
+        }
+    }
+}
